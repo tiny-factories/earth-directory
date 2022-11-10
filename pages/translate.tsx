933230ca74f9903e94edfc8cd77d764f9bd58920
William Felker <gndclouds@icloud.com>
2022-11-10 21:48:28 +0000

    page init

---
+++ pages/translate.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Head from "next/head";
+import Layout from "../components/Layout";
+
+const HelpTranslate: React.FC<TranslateProps> = (props) => {
+  return <></>;
+};
+
+export default HelpTranslate;
