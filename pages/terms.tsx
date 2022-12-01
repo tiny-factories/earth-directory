0acc8d88cec1f564dd21b7b77d4277e8eee6aa14
William Felker <gndclouds@icloud.com>
2022-12-01 18:58:43 +0000

    New Page

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -50,41 +50,26 @@ const Home: React.FC<Props> = (props) => {
 
   return (
     <Layout>
-      <div className="w-full mb-9 sm:py-9 text-h4 sm:text-h3 md:text-h1 font-sans">
-        We at{" "}
-        <Link href="https://madefor.earth">
-          <a className="font-bold hover:underline hover:underline-offset-2">
-            Made For <span className="text-green">Earth</span>
-          </a>
-        </Link>{" "}
-        think that a shared source of truth is required to build a better
-        future. So we started a glossary of terms, technologies, policies, and
-        regulations around climate change.
-      </div>
-
-      {/* <div className="w-full py-9 text-h2">
-        {props.result.length} terms across, {props.result.length} languages,{" "}
-        {props.result.length} Topics
-      </div> */}
-
       <div className="flex w-full justify-between hover:bold">
-       {props.result
-       .sort(function (a, b) {
-         if (a.group < b.group) {
-           return -1;
-         }
-         if (a.group > b.group) {
-           return 1;
-         }
-         return 0;
-       })
-       .map((term, index) => (
-         <div className="" key={index}>
-           <Link href={`#${term.group}`}>
-             <a className="inline-block  text-gray-500 font-satoshi hover:font-bold">{term.group}</a>
-           </Link>         
-         </div>
-       ))}
+        {props.result
+          .sort(function (a, b) {
+            if (a.group < b.group) {
+              return -1;
+            }
+            if (a.group > b.group) {
+              return 1;
+            }
+            return 0;
+          })
+          .map((term, index) => (
+            <div className="" key={index}>
+              <Link href={`#${term.group}`}>
+                <a className="inline-block  text-gray-500 font-satoshi hover:font-bold">
+                  {term.group}
+                </a>
+              </Link>
+            </div>
+          ))}
       </div>
 
       <div className="page">
@@ -103,7 +88,10 @@ const Home: React.FC<Props> = (props) => {
               })
               .map((term, index) => (
                 <div className="" key={index}>
-                  <div id={term.group} className="text-h2 font-bold text-gray-500 font-satoshi">
+                  <div
+                    id={term.group}
+                    className="text-h2 font-bold text-gray-500 font-satoshi"
+                  >
                     {term.group}
                   </div>
                   {term.children
