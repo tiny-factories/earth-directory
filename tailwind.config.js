b58e1c234cab472aa3a8da3d3edc3fa7a5caf64a
William Felker <gndclouds@icloud.com>
2023-01-08 21:02:20 +0000

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,5 +39,6 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
     require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
   ],
 };
