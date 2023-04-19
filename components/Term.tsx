99219bac98f67654118978fc4824d4621a278e71
William Felker <gndclouds@icloud.com>
2023-04-19 23:44:49 +0000

    Added Sponsor flag

--- components/Term.tsx
+++ components/Term.tsx
@@ -6,17 +6,28 @@ export type TermProps = {
   title: string;
   content: string;
   published: boolean;
+  sponsor: boolean;
   sourceId: string;
 };
 
 const Term: React.FC<{ term: TermProps }> = ({ term }) => {
   return (
     <div
-      className="text-h4 sm:text-h3 md:sm:text-h1 font-satoshi border-t border-black snap-center py-3"
-      onClick={() => Router.push("/terms/[id]", `/terms/${term.id}`)}
+      className="text-h4 sm:text-h3 md:sm:text-h2 font-satoshi border-t border-black snap-center py-3"
+      onClick={() => Router.push("/terms/[title]", `/terms/${term.id}`)}
     >
-      <div className="hover:translate-x-3 transform-gpu hover:duration-200">
-        {term.title} <span className="">→</span>
+      <div className="flex justify-between hover:translate-x-3 transform-gpu hover:duration-200">
+        <div>
+          {term.sponsor ? (
+            <>
+              {term.title} <span className="text-[#FE4E00]">🞾</span>
+            </>
+          ) : (
+            <>{term.title} </>
+          )}
+        </div>
+
+        <span className="">→</span>
       </div>
     </div>
   );
