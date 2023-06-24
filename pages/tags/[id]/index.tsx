79c468ee3e9b973585693e082f4e0df979accb5d
William Felker <gndclouds@icloud.com>
2023-06-23 22:22:03 -0700

    removed next auth + new copy

--- pages/tags/[id]/index.tsx
+++ pages/tags/[id]/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import type { GetStaticProps } from "next";
+import Link from "next/link";
 import Head from "next/head";
 import Layout from "../../../components/Layout";
+// import TagProps from "../../types";
+import Term from "../../../components/Term";
+
 import { TagProps } from "../../../components/Tag";
 import prisma from "../../../lib/prisma";
 
@@ -17,6 +21,7 @@ const Tag: React.FC<TagProps> = (props) => {
   if (!props.published) {
     title = `${title} (Draft)`;
   }
+
   console.log(props);
 
   return (
@@ -29,10 +34,31 @@ const Tag: React.FC<TagProps> = (props) => {
       <Layout>
         <div className="mx-auto max-w-7xl ">
           <div className="text-h4 sm:text-h3 md:sm:text-h1 font-bold font-satoshi border-b-2">
-            {title}
+            Tagged "{title}"
           </div>
-          <div className="grid grid-cols-4 py-9">
-            Future List of words for tag {title}
+          <div className="flex flex-wrap w-full ">
+            <div className="pt-3 pb-9 w-full flex justify-between font-bold text-h3">
+              {props.terms
+                .sort(function (a, b) {
+                  if (a.group < b.group) {
+                    return -1;
+                  }
+                  if (a.group > b.group) {
+                    return 1;
+                  }
+                  return 0;
+                })
+                .map((term, i) => (
+                  <div className="" key={i}>
+                    <Link
+                      href={`#${term.group}`}
+                      className="p-3 text-gray-500 font-satoshi font-normal hover:bg-[#FFF] hover:rounded-lg"
+                    >
+                      {term.group}
+                    </Link>
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </Layout>
@@ -41,9 +67,17 @@ const Tag: React.FC<TagProps> = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const tag = await prisma.tag.findFirst({
+  const tag = await prisma.tag.findUnique({
     where: {
       id: String(params?.id),
+    },
+    include: {
+      terms: {
+        select: {
+          title: true,
+          id: true,
+        },
+      },
     },
   });
   return {
